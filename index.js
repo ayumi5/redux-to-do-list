40d1ea1d32ff3b40d98280da9d82735d11ec0004
ayumi5 <ayumi.highroof@gmail.com>
2015-08-31 12:02:38 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import 'babel-core/polyfill';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import todoApp from './reducers';
+
+let store = createStore(todoApp);
+
+let rootElement = document.getElementById('root');
+React.render(
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
+  rootElement
+);
